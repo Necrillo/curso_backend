74fbba115e9f1e0de602053535ab43886aaf69ac
Mario Gonzalez <mariogonzalezsepulveda@gmail.com>
2021-06-26 17:00:21 -0400

    primer commit

---
+++ html/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express ();
+
+app.use('/public',express.static('assets'));
+
+app.get ('/', function(req,res){
+  res.sendFile('index.html',{
+    root: __dirname
+  });
+  //res.send(__dirname);
+});
+
+app.listen(3000);
